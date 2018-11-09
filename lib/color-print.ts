3954cbb6c84ca38230a70c6afb99ae1de1b8ea35
sam_undefined <sam.undefined.dev@gmail.com>
2018-11-09 12:16:00 -0600

    Convert to typescript and imrpove code quality

--- lib/color-print.ts
+++ lib/color-print.ts
@@ -1,20 +1,20 @@
 /**
- * Colorizing function that can be used to color text in the terminal.
+ * Colorizing functions that can be used to color text in the terminal.
  */
 
 const RED = "\x1b[31m%s";
 const GREEN = "\x1b[32m%s";
 const RESET_COLOR = "\x1b[0m";
 
-function print(color, text) {
+function print(color: string, text: string) {
     return console.log(color + RESET_COLOR, text)
 }
 
-module.exports = {
-    red: function (text) {
+export default {
+    red: function (text: string) {
         return print(RED, text)
     },
-    green: function (text) {
+    green: function (text: string) {
         return print(GREEN, text)
     }
 };
---
+++ lib/index.ts
@@ -0,0 +1,17 @@
+#!/usr/bin/env node
+import * as program from 'commander'
+import {tag, unknown} from "./commands"
+
+program
+    .version('0.0.4', '-v, --version')
+    .command('tag <release_number> <name>')
+    .description('Create a git tag following the specified format: yyyy-mm-dd.<release number today>.awesome_and_descriptive_tag_name')
+    .action(tag)
+
+program.on('command:*', unknown)
+
+const noCommandGiven = !process.argv.slice(2).length
+
+if (noCommandGiven) program.outputHelp()
+
+program.parse(process.argv)

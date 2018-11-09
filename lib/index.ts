a04e35e375fd238a7a1b151be5959a2d06e6ac72
sam_undefined <sam.undefined.dev@gmail.com>
2018-11-09 12:16:13 -0600

    Version 0.0.5

--- lib/index.ts
+++ lib/index.ts
@@ -3,7 +3,7 @@ import * as program from 'commander'
 import {tag, unknown} from "./commands"
 
 program
-    .version('0.0.4', '-v, --version')
+    .version('0.0.5', '-v, --version')
     .command('tag <release_number> <name>')
     .description('Create a git tag following the specified format: yyyy-mm-dd.<release number today>.awesome_and_descriptive_tag_name')
     .action(tag)
